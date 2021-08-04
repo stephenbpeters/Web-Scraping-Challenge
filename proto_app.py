2e68ffebd0ba96ba6df8c8104f40c52f11bae633
stephenbpeters <stephen.peters@gmail.com>
2021-08-03 17:18:29 -0700

    fixes

--- proto_app.py
+++ proto_app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 #app.config["MONGO_URI"] = "mongodb://localhost:27017/phone_app"
 app.config["MONGO_URI"] = "mongodb://localhost:27017/proto_app"
 mongo = PyMongo(app)
+m_data = mongo.db.m_data
 
 # Or set inline
 # mongo = PyMongo(app, uri="mongodb://localhost:27017/phone_app")
@@ -15,15 +16,14 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    m_data = mongo.db.m_data.find_one()
-    return render_template("index.html", m_data=m_data)
+    db_data = m_data.find_one()
+    return render_template("index.html", m_data=db_data)
 
 
 @app.route("/scrape")
 def scraper():
-    m_data = mongo.db.m_data
-    m_data_data = proto_scrape.scrape()
-    m_data.update({}, m_data_data, upsert=True)
+    scraped_data = proto_scrape.scrape()
+    m_data.update({}, scraped_data, upsert=True)
     return redirect("/", code=302)
 
 
